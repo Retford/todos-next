1eddae73392029257a4102fcd9ffbd6f1e597000
Retford <retfordjb@gmail.com>
2025-01-11 18:57:40 -0500

    GET, POST, PUT added

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,54 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+import * as yup from 'yup';
+
+interface Segmentes {
+  params: Promise<{ id: string }>;
+}
+export async function GET(request: Request, { params }: Segmentes) {
+  const { id } = await params;
+
+  const todo = await prisma.todo.findFirst({ where: { id: id } });
+
+  if (!todo) {
+    return NextResponse.json(
+      { mg: `No existe un todo con ese id: ${id}` },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segmentes) {
+  const { id } = await params;
+
+  const todo = await prisma.todo.findFirst({ where: { id: id } });
+
+  if (!todo) {
+    return NextResponse.json(
+      { mg: `No existe un todo con ese id: ${id}` },
+      { status: 404 }
+    );
+  }
+
+  try {
+    const { complete, description } = await putSchema.validate(
+      await request.json()
+    );
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id: id },
+      data: { complete, description },
+    });
+
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
+}
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import * as yup from 'yup';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -22,4 +23,23 @@ export async function GET(request: Request) {
 
   const todos = await prisma.todo.findMany({ take: take, skip: skip });
   return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
+
+export async function POST(request: Request) {
+  try {
+    const { complete, description } = await postSchema.validate(
+      await request.json()
+    );
+
+    const todo = await prisma.todo.create({ data: { description, complete } });
+
+    return NextResponse.json(todo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
