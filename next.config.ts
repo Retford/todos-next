9869061e0f0a7dcca9233c55bb96c2dbf6110acb
Retford <retfordjb@gmail.com>
2025-01-28 21:13:17 -0500

    auth github added

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'randomuser.me' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'randomuser.me' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+    ],
   },
 };
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,6 +10,9 @@ import {
   IoListOutline,
 } from 'react-icons/io5';
 import { PiCookieDuotone } from 'react-icons/pi';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 interface MenuItems {
   path: string;
@@ -45,7 +48,19 @@ const menuItems: MenuItems[] = [
   },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/api/auth/signin|');
+  }
+
+  const avatarUrl = session.user?.image
+    ? session.user.image
+    : 'https://randomuser.me/api/portraits/men/4.jpg';
+
+  const userName = session.user?.name ?? 'No name';
+
   return (
     <aside className='ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]'>
       <div>
@@ -63,14 +78,14 @@ export const Sidebar = () => {
 
         <div className='mt-8 text-center'>
           <Image
-            src='https://randomuser.me/api/portraits/men/4.jpg'
-            alt=''
-            className='w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28'
-            width={40}
-            height={40}
+            src={avatarUrl}
+            alt={userName}
+            width={500}
+            height={500}
+            className='w-10 h-10 m-auto rounded-full object-contain lg:w-28 lg:h-28'
           />
           <h5 className='hidden mt-4 text-xl font-semibold text-gray-600 lg:block'>
-            Cynthia J. Watts
+            {userName}
           </h5>
           <span className='hidden text-gray-400 lg:block'>Admin</span>
         </div>
